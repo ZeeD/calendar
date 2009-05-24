fb151fbba6772920bd5a5e9844c0321cf2a176e4
Vito De Tullio <Vito.DeTullio@gmail.com>
2009-05-24 13:58:09 +0200

    Minor fix
    calendar.sql
      * check if the tables to drop exists

--- calendar.sql
+++ calendar.sql
@@ -1,6 +1,6 @@
 begin transaction;
 
-drop table clients;
+drop table if exists clients;
 create table clients (
     client varchar not null,
     machine varchar not null,
@@ -8,7 +8,7 @@ create table clients (
     deltamonth integer not null,
     anticiped bool not null);
 
-drop table payments;
+drop table if exists payments;
 create table payments (
     clients_client varchar references clients(client) not null,
     clients_machine varchar references clients(machine) not null,
