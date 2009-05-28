5281b36be1eb5420735bbe7fda3d71552800ae41
Vito De Tullio <Vito.DeTullio@gmail.com>
2009-05-28 09:18:46 +0200

    Now I can write into a QAbstractTableModel subclass!
    TEST_delegate.py
    * Implemented CustomModel.flags()
    * Some fixes in CustomModel.setData() to send data infos to the delegate
    * now TEST_delegate.py can rest in peace

--- TEST_delegate.py
+++ TEST_delegate.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 
-from PyQt4.QtGui import QStandardItemModel, QItemDelegate, QSpinBox
-from PyQt4.QtCore import QVariant, Qt
+from PyQt4.QtGui import QItemDelegate, QSpinBox
+from PyQt4.QtCore import QVariant, Qt, QAbstractTableModel
 
-class CustomModel(QStandardItemModel):
+class CustomModel(QAbstractTableModel):
     def __init__(self, w=3, h=4):
-        QStandardItemModel.__init__(self, w, h)
-        for r in range(w):
-            for c in range(h):
-                QStandardItemModel.setData(self, self.index(r, c),
-                        QVariant((r+1)*(c+1)))
+        QAbstractTableModel.__init__(self)
+        self._data = [ [ (r+1)*(c+1) for c in range(h) ] for r in range(w) ]
+        self._w = w
+        self._h = h
+
+    def rowCount(self, parent=None):
+        return self._w
+
+    def columnCount(self, parent=None):
+        return self._h
+
+    def data(self, index, role=None):
+        if not index.isValid() or role not in (Qt.DisplayRole, Qt.EditRole):
+            return QVariant()
+        return QVariant(self._data[index.row()][index.column()])
+
+    def setData(self, index, value, role=None):
+        self._data[index.row()][index.column()] = value
+        return True
+
+    def flags(self, index):
+        if index.isValid():
+            return Qt.ItemIsEditable | Qt.ItemIsSelectable | Qt.ItemIsEnabled
+        else:
+            return Qt.NoItemFlags
 
 class CustomDelegate(QItemDelegate):
     def createEditor(self, parent, option, index):
